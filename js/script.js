ae4855f9baa3e047e4d9d087cb1cb4af4a80cc01
elenammm <mallaevaef@gmail.com>
2019-03-27 18:03:41 +0300

    final

--- js/script.js
+++ js/script.js
@@ -1,18 +1,11 @@
 var link = document.querySelector(".interested-button");
 var popup = document.querySelector(".booking");
 	popup.classList.remove("booking-show");
-	link.addEventListener("click", function (evt) {
-	    evt.preventDefault();
-    });
-	popup.classList.toggle("booking-show");
+	link.addEventListener("click", function(evt) { 
+        evt.preventDefault(); 
+        popup.classList.toggle("booking-show");
+        });
 	close.addEventListener("click", function (evt) {
         evt.preventDefault();
         popup.classList.remove("booking-show");
  	    });
-var mapPopup = document.querySelector(".map");
-var mapLink = document.querySelector(".map");
-	mapPopup.classList.remove("booking-show");
-	mapLink.addEventListener("click", function (evt) {
-   		evt.preventDefault();
-    	mapPopup.classList.toggle("booking-show");
-  		});
